870b8b2e29980e292bcb359e0da9c0dd151cb3bc
Inndy <inndy.mail@gmail.com>
2015-02-23 02:01:35 +0800

    Add build tool

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 target-folder" >&2
+    exit 1
+fi
+
+target="$1"
+
+if [ "${target: -1}" = "/" ]; then
+    target="${target:0: -1}"
+fi
+
+if [ "${target:0:1}" = "/" ]; then
+    target="${target:1}"
+fi
+
+mkdir build
+cp -R ${target}/* build/
+./remotify_resoure.py "http://sitcon.org/newsletter/${target}/" build/index.html
+./inline-css.py build/index.out.html build/index.html
+rm build/index.out.html
+tar -C build -zcvf build.tar.gz .
